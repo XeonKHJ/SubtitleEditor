24347f797986ec51054a96e266d99b0f36500ec6
康宏嘉 <redalertkhj@live.cn>
2020-02-20 16:45:07 +0800

    完善解析SubRip的正则表达式。

--- SubtitleEditor.Subtitle/SubRipParser.cs
+++ SubtitleEditor.Subtitle/SubRipParser.cs
@@ -19,15 +19,13 @@ namespace SubtitleEditor.Subtitle
 
         public Subtitle LoadFromString(string subString)
         {
-            Regex beginOrNewLineRegex = new Regex(@"^|((\s*)(\r|\n)+)");
             Regex numberLineRegex = new Regex(@"(?<number>\d+)");
             Regex beginTimeRegex = new Regex(@"(?<beginTime>(\d{2}):(\d{2}):(\d{2}),(\d{3}))");
             Regex endTimeRegex = new Regex(@"(?<endTime>(\d{2}):(\d{2}):(\d{2}),(\d{3}))");
             Regex timeArrowRegex = new Regex("(-->)");
             Regex newLineRegex = new Regex(@"(\n|\r)+");
-            Regex dialogueLineRegex = new Regex(@"((?<dialogue>.*?)(?=(((\r|\n)+\d+\s*(\r|\n)+((\d{2}):(\d{2}):(\d{2}),(\d{3}))(\s*)(-->)(\s*)((\d{2}):(\d{2}):(\d{2}),(\d{3})))|\z)))");
+            Regex dialogueLineRegex = new Regex(@"((?<dialogue>.*?)(?=(((\s*)(\r|\n)+\d+\s*(\r|\n)+((\d{2}):(\d{2}):(\d{2}),(\d{3}))(\s*)(-->)(\s*)((\d{2}):(\d{2}):(\d{2}),(\d{3})))|(\s*(\r|\n)*)\z)))");
             Regex anyEmptyChar = new Regex(@"(\s)*");
-            Regex contRegex = new Regex(@"(?<dialogue>(.*))", RegexOptions.Singleline);
             var fullRegex = new Regex(numberLineRegex.ToString() //出现序号
                                     + anyEmptyChar.ToString() //可以有任意空格
                                     + newLineRegex.ToString() //换行
@@ -38,9 +36,12 @@ namespace SubtitleEditor.Subtitle
                                     + endTimeRegex.ToString() //结束时间
                                     + anyEmptyChar.ToString() //任意空格
                                     + newLineRegex.ToString() //换行
-                                    + dialogueLineRegex.ToString()
+                                    + dialogueLineRegex.ToString() //台词
                                     , RegexOptions.Singleline
                                     );
+
+
+            Subtitle subtitle = new Subtitle();
 
             return new Subtitle();
         }
